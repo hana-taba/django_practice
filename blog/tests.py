d74f111bf68ded306715f846c7003546fe677401
jinsik <jinsikshin21@gmail.com>
2024-06-27 15:31:37 +0900

    포스트 목록 상세 페이제에서 작성자 정보 출력 git push

--- blog/tests.py
+++ blog/tests.py
@@ -1,10 +1,14 @@
 from django.test import TestCase, Client
 from bs4 import BeautifulSoup
 from .models import Post
+from django.contrib.auth.models import User
+
 
 class TestView(TestCase):
     def setUp(self):
         self.client = Client()
+        self.trump=User.objects.create_user(username='trump',password='somepassword')
+        self.user_obama=User.objects.create_user(username='obama',password='somepassword')
 
     def navbar_test(self, soup):
         navbar=soup.nav
@@ -34,10 +38,12 @@ class TestView(TestCase):
         post_001 = Post.objects.create(
             title='첫번째 포스트 입니다',
             content='Hello World. We are the world',
+            author=self.user_trump
         )
         post_002 = Post.objects.create(
             title='두번째 포스트입니다',
             content='1등이 전부는 아니잖아',
+            author=self.user_obama
         )
         self.assertEqual(Post.objects.count(), 2)
 
@@ -49,10 +55,15 @@ class TestView(TestCase):
         self.assertIn(post_001.title, main_area.text)
         self.assertIn(post_002.title, main_area.text)
         self.assertNotIn('아직 게시물 없습니다', main_area.text)
+
+        self.assertIn(self.user_trump.usename.upper(),main_area.text)
+        self.assertIn(self.user_obama.username.upper(),main_area.text)
+
     def test_post_deatil(self):
         post_001=Post.objects.create(
             title='첫 번째 포스트입니다.',
             content='Hello World. We are the world',
+            author=self.user_trump,
         )
         self.assertEqual(post_001.get_absolute_url(),'/blog/1/')
         response=self.client.get(post_001.get_absolute_url())
@@ -62,6 +73,7 @@ class TestView(TestCase):
         self.assertIn(post_001.title,soup.title.text)
         main_area=soup.find('div',id='main-area')
         post_area=main_area.find('div',id='post-area')
+        self.assertIn(self.user_trump.username.upper(),post_area.text)
         self.assertIn(post_001.title,post_area.text)
         self.assertIn(post_001.content,post_area.text)
 
