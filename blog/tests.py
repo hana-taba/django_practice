2bc210a42b7e79d002028d1c0c941b64304e5141
jinsik <jinsikshin21@gmail.com>
2024-06-27 12:47:11 +0900

    내비게이션 푸터 모듈화

--- blog/tests.py
+++ blog/tests.py
@@ -6,15 +6,26 @@ class TestView(TestCase):
     def setUp(self):
         self.client = Client()
 
+    def navbar_test(self, soup):
+        navbar=soup.nav
+        self.assertIn('Blog',navbar.text)
+        self.assertIn('About me',navbar.text)
+
+        logo_btn=navbar.find('a',text='Do It Django')
+        self.assertEqual(logo_btn.attrs['href'],'/')
+        home_btn=navbar.find('a',text='Home')
+        self.assertEqual(home_btn.attrs['href'], '/')
+        blog_btn = navbar.find('a', text='Blog')
+        self.assertEqual(blog_btn.attrs['href'], '/blog/')
+        about_me_btn=navbar.find('a',text="About me")
+        self.assertEqual(about_me_btn.attrs['href'],'/about_me/')
     def test_post_list(self):
         # Test when there are no posts
         response = self.client.get('/blog/')
         self.assertEqual(response.status_code, 200)
         soup = BeautifulSoup(response.content, 'html.parser')
         self.assertEqual(soup.title.text, 'Blog')
-        navbar = soup.nav
-        self.assertIn('Blog', navbar.text)
-        self.assertIn('About me', navbar.text)
+        self.navbar_test(soup)
         self.assertEqual(Post.objects.count(), 0)
         main_area = soup.find('div', id='main-area')
         self.assertIn('아직 게시물이 없습니다', main_area.text)
@@ -43,13 +54,11 @@ class TestView(TestCase):
             title='첫 번째 포스트입니다.',
             content='Hello World. We are the world',
         )
-        self.assertEqual(post_001.get_absolute_url(),'/blog/')
+        self.assertEqual(post_001.get_absolute_url(),'/blog/1/')
         response=self.client.get(post_001.get_absolute_url())
         self.assertEqual(response.status_code,200)
         soup=BeautifulSoup(response.content,'html.parser')
-        navbar=soup.nav
-        self.assertIn('Blog',navbar.text)
-        self.assertIn('About me',navbar.text)
+        self.navbar_test(soup)
         self.assertIn(post_001.title,soup.title.text)
         main_area=soup.find('div',id='main-area')
         post_area=main_area.find('div',id='post-area')
